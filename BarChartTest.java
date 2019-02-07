0285ed251a12bae2f27d50f3f919855095178fdb
Ab-Daniel <31863068+Ab-Daniel@users.noreply.github.com>
2019-02-07 10:28:32 -0700

    Create BarChartTest.java

---
+++ BarChartTest.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class BarChartTest 
+{
+	
+	public static void main(String[] args) 
+	{
+		
+		JFrame frame = new BarChartFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
+	}
+	
+}
