e01f6ff9fcafa37652ee90b6857875082167a077
Ab-Daniel <31863068+Ab-Daniel@users.noreply.github.com>
2019-05-20 15:33:25 -0700

    Update BarChartTest.java

--- BarChartTest.java
+++ BarChartTest.java
@@ -2,19 +2,10 @@ import javax.swing.JFrame;
 
 public class BarChartTest 
 {
-	
-	public static void main(String[] args) 
-	{
-		
-		JFrame frame = new JFrame();
-		frame.setSize(500, 500);
+	public static void main(String[] args)
+	{  
+		JFrame frame = new BarChartFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setTitle("Bar Chart Program");
 		frame.setVisible(true);
-		
-		BarChartFrame p = new BarChartFrame();
-		frame.add(p);
-		
 	}
-	
 }
